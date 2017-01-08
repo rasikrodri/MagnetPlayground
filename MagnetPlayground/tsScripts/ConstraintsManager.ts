b80234f9da7ec342584ea26421b46dc1c8883cd9
rasikrodri <rasikrodri@gmail.com>
2017-01-07 21:04:10 -0500

    disabled constraints because they have to be tested to make sure they work.

--- MagnetPlayground/tsScripts/ConstraintsManager.ts
+++ MagnetPlayground/tsScripts/ConstraintsManager.ts
@@ -9,43 +9,63 @@
         this.UpdateAllRotZ();
     }
     private UpdateAllRotX() {
-        for (var i = 0; i < this.RotConstrX.length; i++) {
-            var c = this.RotConstrX[i];
-            var currAngles = c.Mesh.rotationQuaternion.toEulerAngles();
-            
-            if (currAngles.x < c.Min) {
-                c.Mesh.rotation.x = c.Min - currAngles.x;
-            }
-            else if (currAngles.x > c.Max) {
-                c.Mesh.rotation.x = -(currAngles.x - c.Max);
-            }
-        }
+        //for (var i = 0; i < this.RotConstrX.length; i++) {
+        //    var c = this.RotConstrX[i];
+        //    c.Mesh.rotation.x = 0;
+
+        //    //var currAngles = c.Mesh.rotationQuaternion.toEulerAngles();                
+        //    //if (currAngles.x < c.Min) {
+        //    //    c.Mesh.rotation.x = c.Min - currAngles.x;
+        //    //}
+        //    //else if (currAngles.x > c.Max) {
+        //    //    c.Mesh.rotation.x = -(currAngles.x - c.Max);
+        //    //}
+
+        //    //Set angular velocity for x to 0
+        //    var impostor = c.Mesh.getPhysicsImpostor();
+        //    var angVelo = impostor.getAngularVelocity();
+        //    angVelo.x = 0;
+        //    impostor.setAngularVelocity(angVelo);
+        //}
     }
     private UpdateAllRotY() {
-        for (var i = 0; i < this.RotConstrY.length; i++) {
-            var c = this.RotConstrX[i];
-            var currAngles = c.Mesh.rotationQuaternion.toEulerAngles();
+        //for (var i = 0; i < this.RotConstrY.length; i++) {
+        //    var c = this.RotConstrY[i];
+        //    c.Mesh.rotation.y = 0;
 
-            if (currAngles.y < c.Min) {
-                c.Mesh.rotation.y = c.Min - currAngles.y;
-            }
-            else if (currAngles.y > c.Max) {
-                c.Mesh.rotation.y = -(currAngles.y - c.Max);
-            }
-        }
+        //    //var currAngles = c.Mesh.rotationQuaternion.toEulerAngles();
+        //    //if (currAngles.y < c.Min) {
+        //    //    c.Mesh.rotation.y = c.Min - currAngles.y;
+        //    //}
+        //    //else if (currAngles.y > c.Max) {
+        //    //    c.Mesh.rotation.y = -(currAngles.y - c.Max);
+        //    //}
+
+        //    //Set angular velocity for y to 0
+        //    var impostor = c.Mesh.getPhysicsImpostor();
+        //    var angVelo = impostor.getAngularVelocity();
+        //    angVelo.y = 0;
+        //    impostor.setAngularVelocity(angVelo);
+        //}
     }
     private UpdateAllRotZ() {
-        for (var i = 0; i < this.RotConstrZ.length; i++) {
-            var c = this.RotConstrX[i];
-            var currAngles = c.Mesh.rotationQuaternion.toEulerAngles();
+        //for (var i = 0; i < this.RotConstrZ.length; i++) {
+        //    var c = this.RotConstrZ[i];
+        //    c.Mesh.rotation.z = 0;
+        //    //var currAngles = c.Mesh.rotationQuaternion.toEulerAngles();
+        //    //if (currAngles.z < c.Min) {
+        //    //    c.Mesh.rotation.z = c.Min - currAngles.z;
+        //    //}
+        //    //else if (currAngles.z > c.Max) {
+        //    //    c.Mesh.rotation.z = -(currAngles.z - c.Max);
+        //    //}
 
-            if (currAngles.z < c.Min) {
-                c.Mesh.rotation.z = c.Min - currAngles.z;
-            }
-            else if (currAngles.z > c.Max) {
-                c.Mesh.rotation.z = -(currAngles.z - c.Max);
-            }
-        }
+        //    //Set angular velocity for z to 0
+        //    var impostor = c.Mesh.getPhysicsImpostor();
+        //    var angVelo = impostor.getAngularVelocity();
+        //    angVelo.z = 0;
+        //    impostor.setAngularVelocity(angVelo);
+        //}
     }
 
     //max is maximun angle, min is minimum angle.
@@ -66,7 +86,7 @@ class RotationConstraint {
     Mesh: BABYLON.Mesh;
     Max: number;
     Min: number;
-    constructor(mesh: BABYLON.Mesh, max: number, min: number) {
+    constructor(mesh: BABYLON.Mesh, min: number, max: number) {
         this.Mesh = mesh;
         this.Max = max * Math.PI / 180.0;
         this.Min = min * Math.PI / 180.0;
--- MagnetPlayground/tsScripts/constraintsmanager.js
+++ MagnetPlayground/tsScripts/constraintsmanager.js
@@ -10,40 +10,58 @@ var ConstraintsManager = (function () {
         this.UpdateAllRotZ();
     };
     ConstraintsManager.prototype.UpdateAllRotX = function () {
-        for (var i = 0; i < this.RotConstrX.length; i++) {
-            var c = this.RotConstrX[i];
-            var currAngles = c.Mesh.rotationQuaternion.toEulerAngles();
-            if (currAngles.x < c.Min) {
-                c.Mesh.rotation.x = c.Min - currAngles.x;
-            }
-            else if (currAngles.x > c.Max) {
-                c.Mesh.rotation.x = -(currAngles.x - c.Max);
-            }
-        }
+        //for (var i = 0; i < this.RotConstrX.length; i++) {
+        //    var c = this.RotConstrX[i];
+        //    c.Mesh.rotation.x = 0;
+        //    //var currAngles = c.Mesh.rotationQuaternion.toEulerAngles();                
+        //    //if (currAngles.x < c.Min) {
+        //    //    c.Mesh.rotation.x = c.Min - currAngles.x;
+        //    //}
+        //    //else if (currAngles.x > c.Max) {
+        //    //    c.Mesh.rotation.x = -(currAngles.x - c.Max);
+        //    //}
+        //    //Set angular velocity for x to 0
+        //    var impostor = c.Mesh.getPhysicsImpostor();
+        //    var angVelo = impostor.getAngularVelocity();
+        //    angVelo.x = 0;
+        //    impostor.setAngularVelocity(angVelo);
+        //}
     };
     ConstraintsManager.prototype.UpdateAllRotY = function () {
-        for (var i = 0; i < this.RotConstrY.length; i++) {
-            var c = this.RotConstrX[i];
-            var currAngles = c.Mesh.rotationQuaternion.toEulerAngles();
-            if (currAngles.y < c.Min) {
-                c.Mesh.rotation.y = c.Min - currAngles.y;
-            }
-            else if (currAngles.y > c.Max) {
-                c.Mesh.rotation.y = -(currAngles.y - c.Max);
-            }
-        }
+        //for (var i = 0; i < this.RotConstrY.length; i++) {
+        //    var c = this.RotConstrY[i];
+        //    c.Mesh.rotation.y = 0;
+        //    //var currAngles = c.Mesh.rotationQuaternion.toEulerAngles();
+        //    //if (currAngles.y < c.Min) {
+        //    //    c.Mesh.rotation.y = c.Min - currAngles.y;
+        //    //}
+        //    //else if (currAngles.y > c.Max) {
+        //    //    c.Mesh.rotation.y = -(currAngles.y - c.Max);
+        //    //}
+        //    //Set angular velocity for y to 0
+        //    var impostor = c.Mesh.getPhysicsImpostor();
+        //    var angVelo = impostor.getAngularVelocity();
+        //    angVelo.y = 0;
+        //    impostor.setAngularVelocity(angVelo);
+        //}
     };
     ConstraintsManager.prototype.UpdateAllRotZ = function () {
-        for (var i = 0; i < this.RotConstrZ.length; i++) {
-            var c = this.RotConstrX[i];
-            var currAngles = c.Mesh.rotationQuaternion.toEulerAngles();
-            if (currAngles.z < c.Min) {
-                c.Mesh.rotation.z = c.Min - currAngles.z;
-            }
-            else if (currAngles.z > c.Max) {
-                c.Mesh.rotation.z = -(currAngles.z - c.Max);
-            }
-        }
+        //for (var i = 0; i < this.RotConstrZ.length; i++) {
+        //    var c = this.RotConstrZ[i];
+        //    c.Mesh.rotation.z = 0;
+        //    //var currAngles = c.Mesh.rotationQuaternion.toEulerAngles();
+        //    //if (currAngles.z < c.Min) {
+        //    //    c.Mesh.rotation.z = c.Min - currAngles.z;
+        //    //}
+        //    //else if (currAngles.z > c.Max) {
+        //    //    c.Mesh.rotation.z = -(currAngles.z - c.Max);
+        //    //}
+        //    //Set angular velocity for z to 0
+        //    var impostor = c.Mesh.getPhysicsImpostor();
+        //    var angVelo = impostor.getAngularVelocity();
+        //    angVelo.z = 0;
+        //    impostor.setAngularVelocity(angVelo);
+        //}
     };
     //max is maximun angle, min is minimum angle.
     ConstraintsManager.prototype.CreateRotationConstraintX = function (mesh, max, min) {
@@ -60,7 +78,7 @@ var ConstraintsManager = (function () {
     return ConstraintsManager;
 }());
 var RotationConstraint = (function () {
-    function RotationConstraint(mesh, max, min) {
+    function RotationConstraint(mesh, min, max) {
         this.Mesh = mesh;
         this.Max = max * Math.PI / 180.0;
         this.Min = min * Math.PI / 180.0;
